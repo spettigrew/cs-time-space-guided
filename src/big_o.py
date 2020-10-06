357d6e8b9265ac9d5c48ac5ab67991e6c4259990
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-06 13:08:35 -0600

    Notes added to bigO notation.

--- src/big_o.py
+++ src/big_o.py
@@ -5,10 +5,10 @@
 # different computers run at different speeds
 # efficiency
 
-# O(n) --> linear
-# O(n^2) -- quadratic
-# O(1) -- constant -- the performance doesn't change regardless of input
-# O(log n) -- logarithmic -- every time we double the input size, we only add one extra step
-# O(2^n) -- exponential
+# O(n) --> linear time. Algo increases the number of operations it performs as long as it performs as long a log-linear function of the input size n
+# O(n^2) -- quadratic. Squared - Also increases the number of operations it performs quad function of the input size n
+# O(1) -- constant -- the performance doesn't change regardless of input. Best Runtime. Any input size, it performs the same number of operations.
+# O(log n) -- logarithmic -- every time we double the input size, we only add one extra step. Number of operation performs as log function of the input size n. Binary search.
+# O(2^n) -- exponential. Worst Runtime. Number of operations it performs as input size increases is limited.
 # factorial
 # O(n log n) -- linearithmic
