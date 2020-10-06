c5200ab9ac5c5993997f2568a1902cade88fa4fd
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-05 18:46:04 -0600

    Notes added about big0 complexity

---
+++ src/big_o.py
@@ -0,0 +1,14 @@
+
+# why do we have big O notation?
+# To evaluate performance.
+# to find out the time it takes to complete a function (releative to other input sizes) 
+# different computers run at different speeds
+# efficiency
+
+# O(n) --> linear
+# O(n^2) -- quadratic
+# O(1) -- constant -- the performance doesn't change regardless of input
+# O(log n) -- logarithmic -- every time we double the input size, we only add one extra step
+# O(2^n) -- exponential
+# factorial
+# O(n log n) -- linearithmic
