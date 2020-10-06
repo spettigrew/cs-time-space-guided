b8239b8b01f021c1238682aca669c0b001b63b60
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-06 14:11:19 -0600

    Classification and Description added to notes

--- src/big_o.py
+++ src/big_o.py
@@ -12,3 +12,12 @@
 # O(2^n) -- exponential. Worst Runtime. Number of operations it performs as input size increases is limited.
 # factorial
 # O(n log n) -- linearithmic
+
+
+# Classification	    Description
+# Constant O(1)	The runtime is entirely unaffected by the input size. This is the ideal solution.
+# Logarithmic O(log n)	As the input size increases, the runtime will grow slightly slower. This is a pretty good solution.
+# Linear O(n)	As the input size increases, the runtime will grow at the same rate. This is a pretty good solution.
+# Polynomial O(n^c)	As the input size increases, the runtime will grow at a faster rate. This might work for small inputs but is not a scalable solution.
+# Exponential O(c^n)	As the input size increases, the runtime will grow at a much faster rate. This solution is inefficient.
+# Factorial O(n!)	As the input size increases, the runtime will grow astronomically, even with relatively small inputs. This solution is exceptionally inefficient.
